391c1ae19c64e883e89faaa4b9b88c6bb47ff1b0
Gale04 <studente.galeotti.marco@gmail.com>
2017-11-14 14:21:05 +0100

    es lista spesa

---
+++ Esempi/EsToDoList/app.js
@@ -0,0 +1,86 @@
+window.onload = function()
+{
+
+new Vue (
+{
+    el:'#app', 
+    data: 
+    {
+
+            numProdottiSelezionati: 0,
+
+            nuovoProdotto: "",
+
+            listaSpesa: [
+                {
+                    preso: false,
+                    testo: "Latte"
+                }, 
+                {
+                    preso: false,
+                    testo: "Miele"
+                }, 
+                {
+                    preso: false,
+                    testo: "patatine"
+                }]
+        
+    },
+
+    methods:
+    {
+       aggiuntaProdotto: function()
+       {
+           if(this.nuovoProdotto != "")
+           {
+            this.listaSpesa.push(
+                {
+                    preso: false, 
+                    testo: this.nuovoProdotto 
+                }
+            );
+            this.nuovoProdotto = "";
+           }
+    
+           
+       },
+
+       aggiornamentoChecked: function(prodotto)
+       {
+           prodotto.preso = !prodotto.preso;
+           if(prodotto.preso ? this.numProdottiSelezionati++ :  this.numProdottiSelezionati--);
+       },
+
+       eliminazioneProdottiSelezionati: function()
+       {
+           
+           if(window.confirm("Confermi l'eleminazione di " + this.numProdottiSelezionati + " prodotti?"))
+           {
+               var nuovaLista = new Array;
+
+               for(var prodotto of this.listaSpesa){
+                   if(!prodotto.preso){
+                       nuovaLista.push(prodotto);
+                   }
+                }
+                
+                this.listaSpesa = nuovaLista;
+                this.numProdottiSelezionati = 0;
+           }
+
+          
+        },
+        prodottiInLista: function()
+        {
+                return 
+        }
+
+    }
+
+
+
+
+    }
+)
+
+}
