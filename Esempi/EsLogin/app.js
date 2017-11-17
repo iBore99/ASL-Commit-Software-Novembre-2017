17518b58f416d628b531a0e07fdac9401f2bd4be
Gale04 <studente.galeotti.marco@gmail.com>
2017-11-17 16:51:36 +0100

    modifiche all'esercizio del login

--- Esempi/EsLogin/app.js
+++ Esempi/EsLogin/app.js
@@ -1,22 +1,77 @@
 window.onload = function() {
-    (function() {
-        localStorage.setItem('username', 'pippo');
-        localStorage.setItem('password', '123');
-    });
-
+  
     new Vue({
 
         el: '#app',
 
         data: {
-            username: "",
-            password: "",
+
+           
+            nuovoUsername:"",
+            nuovaPassword:"",
+            stringaJson: "",
+            controlloPulsante: true,
+
+            utenti: []       
         },
 
         methods: {
 
-        }
+            inizializzaDati(){
+                this.stringaJson = localStorage.getItem('utenti');
+                if(this.stringaJson)
+                  this.utenti = this.deserializzaObjDaJson(this.stringaJson);
+            },
+            aggiuntaUtente(){          //metodo per la registrazione di un nuovo utente
 
-    })
+                    if(this.stringaJson)
+                        this.utenti = this.deserializzaObjDaJson(this.stringaJson);
+                    
+                    this.utenti.push(
+                        {
+                            username: this.nuovoUsername,
+                            password: this.nuovaPassword
+                        });
+
+                        
+                        this.nuovoUsername="";
+                        this.nuovaPassword="";
+                        this.stringaJson = this.serializzaObjInJson(this.utenti);
+                        localStorage.setItem("utenti", this.stringaJson);                  
+              
+            },
+
+            cambiaStato()
+            {
+                this.controlloPulsante = !this.controlloPulsante;   //cambia il valore della variabile 
+                this.nuovaPassword="";
+                this.nuovoUsername="";
+            },
+
+            confrontaCredenziali()
+             {
+
+                for(var utente of this.utenti){
+                    if(this.nuovoUsername==utente.username && this.nuovaPassword==utente.password)
+                    {
+                        alert("Autenticazione avvenuta con successo.");
+                        return;
+                    }
+                }
+
+                alert("Autenticazione fallita!");
+             },
+
+             serializzaObjInJson(obj){      //creazione stringa json
+                return JSON.stringify(obj);
+              },
+      
+              deserializzaObjDaJson(stringa_json) {   //ricavo oggetto da stringa json
+                    return eval(stringa_json);
+              }
+           
+        }  //fine methods
+
+    })   //fine vue
 
 }
