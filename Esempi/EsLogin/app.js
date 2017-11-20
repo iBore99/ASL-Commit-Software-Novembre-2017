6f8880296adfc9feffda55c984ef8562e70554ae
iBore99 <studente.borelli.cristian@gmail.com>
2017-11-20 09:56:21 +0100

    Aggiustamento reg Expression per password

--- Esempi/EsLogin/app.js
+++ Esempi/EsLogin/app.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  
+
     new Vue({
 
         el: '#app',
@@ -8,117 +8,118 @@ window.onload = function() {
 
             /*Variabili flag che mi servono per decidere cosa mostrare 
             nella pagina in base aai dati inseriti*/
-         
-            usernameValido: true,   
+
+            usernameValido: true,
             passwordValida: true,
-            
-            nuovoUsername:"",
-            nuovaPassword:"",
+
+            nuovoUsername: "",
+            nuovaPassword: "",
             stringaJson: "",
             controlloPulsante: true,
 
-            utenti: []       
+            utenti: []
         },
 
         methods: {
 
-            inizializzaDati(){
+            inizializzaDati() {
                 this.stringaJson = localStorage.getItem('utenti');
-                if(this.stringaJson)
-                  this.utenti = this.deserializzaObjDaJson(this.stringaJson);
+                if (this.stringaJson)
+                    this.utenti = this.deserializzaObjDaJson(this.stringaJson);
             },
 
-            validazioneDati(){
+            validazioneDati() {
                 return (this.passwordValida && this.usernameValido)
 
-             },
+            },
 
-            controlloPassword(){
-                
-                    if(
-                        /^[a-zA-Z0-9]{6,16}$/.test(this.nuovaPassword) &&
+            controlloPassword() {
+
+                if (this.nuovaPassword) {
+                    /*è necessario questo controllo perchè veniva eseguito il controllo sulla password anche quando non veniva 
+                    scritto nulla, per esempio bastava che si fosse premuto il tasto "tab" per passare a quella casella di testo.*/
+                    if (
+                        /^[a-zA-Z0-9?!$+-/.,@^_]{6,16}$/.test(this.nuovaPassword) &&
                         /[A-Z]/.test(this.nuovaPassword) &&
                         /[a-z]/.test(this.nuovaPassword) &&
-                        /[0-9]/.test(this.nuovaPassword) 
-                    ){
+                        /[0-9]/.test(this.nuovaPassword)
+                    ) {
 
                         this.passwordValida = true;
                         return true;
-                    }
-                    else this.passwordValida = false;
+                    } else this.passwordValida = false;
 
                     return false;
-                    
-             },
+                } else {
+                    this.passwordValida = true;
+                }
 
-            controlloUsername(){
-                 for(var utente of this.utenti){
-                     if(utente.username == this.nuovoUsername){
+            },
+
+            controlloUsername() {
+                for (var utente of this.utenti) {
+                    if (utente.username == this.nuovoUsername) {
                         this.usernameValido = false;
                         return false;
-                     }
-                 }
-                 
+                    }
+                }
 
-                 this.usernameValido = true;
-                 return true;
-             },
 
-            aggiuntaUtente(){          //metodo per la registrazione di un nuovo utente
+                this.usernameValido = true;
+                return true;
+            },
 
-                if(this.validazioneDati()){
-                    if(this.stringaJson)
+            aggiuntaUtente() { //metodo per la registrazione di un nuovo utente
+
+                if (this.validazioneDati()) {
+                    if (this.stringaJson)
                         this.utenti = this.deserializzaObjDaJson(this.stringaJson);
-                    
-                
-                    
-                    this.utenti.push(
-                        {
-                            username: this.nuovoUsername,
-                            password: this.nuovaPassword
-                        });
 
-                        
+
+
+                    this.utenti.push({
+                        username: this.nuovoUsername,
+                        password: this.nuovaPassword
+                    });
+
+
                     this.stringaJson = this.serializzaObjInJson(this.utenti);
-                    localStorage.setItem("utenti", this.stringaJson);       
-                        
-                    this.nuovoUsername="";
-                    this.nuovaPassword="";
+                    localStorage.setItem("utenti", this.stringaJson);
+
+                    this.nuovoUsername = "";
+                    this.nuovaPassword = "";
                     alert("Registrazione avvenuta con successo!");
                 }
             },
 
-            cambiaStato()
-            {
-                this.controlloPulsante = !this.controlloPulsante;   //cambia il valore della variabile 
-                this.nuovaPassword="";
-                this.nuovoUsername="";
+            cambiaStato() {
+                this.controlloPulsante = !this.controlloPulsante; //cambia il valore della variabile 
+                this.nuovaPassword = "";
+                this.nuovoUsername = "";
             },
 
-            confrontaCredenziali()
-             {
+            confrontaCredenziali() {
 
-                for(var utente of this.utenti){
-                    if(this.nuovoUsername==utente.username && this.nuovaPassword==utente.password)
-                    {
+                for (var utente of this.utenti) {
+                    if (this.nuovoUsername == utente.username && this.nuovaPassword == utente.password) {
                         alert("Autenticazione avvenuta con successo.");
                         return;
                     }
                 }
 
                 alert("Autenticazione fallita!");
-             },
+            },
 
-             serializzaObjInJson(obj){      //creazione stringa json
+            serializzaObjInJson(obj) { //creazione stringa json
                 return JSON.stringify(obj);
-              },
-      
-              deserializzaObjDaJson(stringa_json) {   //ricavo oggetto da stringa json
-                    return eval(stringa_json);
-              }
-           
-        }  
+            },
 
-    }) 
+            deserializzaObjDaJson(stringa_json) { //ricavo oggetto da stringa json
+                return eval(stringa_json);
+            }
+
+        }
+
+    })
 
 }
